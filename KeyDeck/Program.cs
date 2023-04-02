477370223443a0503ff9641a0431637b20ca11fc
James <redthunderspartan@gmail.com>
2023-04-01 22:11:15 -0500

    changes

--- KeyDeck/Program.cs
+++ KeyDeck/Program.cs
@@ -5,17 +5,27 @@ namespace KeyDeck
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        /// Runs on initalization
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Bind());
+            Application.Run(new Home());
 
+            //Initalization functions
+            SerialCommunication.Initalize();
+            ProgramProperties.Initalize();
+
+            //Initalize events
+            AppDomain.CurrentDomain.ProcessExit += new EventHandler(OnProcessExit);
 
         }
+
+        static void OnProcessExit(object sender, EventArgs e)
+        {
+            //Run functions on application exit
+        }
+
     }
 }
---
+++ KeyDeck/ProgramProperties.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+
+namespace KeyDeck
+{
+    internal class ProgramProperties
+    {
+
+        //All the bind properties
+        public static String bind1 { get; set; }
+        public static String bind2 { get; set; }
+        public static String bind3 { get; set; }
+        public static String bind4 { get; set; }
+        public static String bind5 { get; set; }
+        public static String bind6 { get; set; }
+        public static String bind7 { get; set; }
+        public static String bind8 { get; set; }
+        public static String bind9 { get; set; }
+        static String[] binds = {bind1, bind2, bind3, bind4, bind5, bind6, bind7, bind8, bind9};
+
+        public static void Initalize()
+        {
+
+            
+
+        }
+
+        public static void saveProperties()
+        {
+            //Save binds to a JSON file
+            foreach (String bind in binds)
+            {
+                string json = JsonConvert.SerializeObject(bind, Newtonsoft.Json.Formatting.Indented);
+                File.WriteAllText(@"c:\user.json", json);
+            }
+        }
+    }
+}
